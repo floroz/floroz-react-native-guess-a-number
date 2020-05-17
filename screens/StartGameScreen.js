3389e8ff9b17a88a36886d25042dc4a11729058b
Daniele Tortora <daniele.tortora@secretescapes.com>
2020-05-17 12:56:49 +0100

    styling overlay

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,12 +4,12 @@ import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text style={styles.title}>Test</Text>
+      <Text style={styles.title}>Start a New Game</Text>
       <View style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
+          <Button title="Reset" onPress={() => {}} color="red" />
           <Button title="Confirm" onPress={() => {}} />
         </View>
       </View>
@@ -30,7 +30,18 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: 300,
     maxWidth: "80%",
+    padding: 20,
     alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+    shadowRadius: 5,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: "white",
+    borderRadius: 20,
   },
   buttonContainer: {
     flexDirection: "row",
